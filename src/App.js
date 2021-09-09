fac781fc91fbb48e476c699045ef1a5435cb6eff
fpem3309 <alsrhks2154@gmail.com>
2021-09-09 17:10:56 +0900

    loading

--- src/App.js
+++ src/App.js
@@ -1,45 +1,22 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import axios from 'axios';
 
 class App extends React.Component{
-  constructor(props){
-    super(props);
-    console.log("I'm constructor");
-  }
-
   state ={
-    count : 0
+    isLoading : true,
+    movies : []
   };
 
-
-  add = () => {
-    this.setState(current => ({ count : current.count +1}));
-  };
-
-  minus = () => {
-    this.setState(current => ({ count : current.count -1}));
-  };
-  componentDidMount(){
-    console.log("I'm component did mount");
-  }
-  componentDidUpdate(){
-    console.log("I'm did update");
-  }
-  componentWillUnmount(){
-    console.log("I'm will unmount");
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading : false});
+    }, 5000);
   }
 
   render(){
-    console.log("I'm rendering");
-    return(
-      <div>
-       <h1>The number is {this.state.count}</h1>
-       <button onClick={this.add}>Add</button>
-       <button onClick={this.minus}>minus</button>
-      </div>
-      );
+    const {isLoading} = this.state;
+     return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
   }
-
 }
 
 export default App;
