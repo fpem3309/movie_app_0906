491c4ea152c0fa080f5d01048a704a3ec0678d64
fpem3309 <alsrhks2154@gmail.com>
2021-09-10 16:01:25 +0900

    before html

--- src/App.js
+++ src/App.js
@@ -24,15 +24,21 @@ class App extends React.Component{
 
   render(){
     const {isLoading, movies } = this.state;
-     return( <div>{isLoading ? "Loading..." : movies.map(movie =>(
-        <Movie id={movie.id} // props들 가져오기
-          key={movie.id}
-          year={movie.year}
-          title={movie.title}
-          summary={movie.summary}
-          poster={movie.medium_cover_image}/>
-          ))}
-      </div>
+     return(
+      <section class="container">
+        {isLoading
+        ? <div class="loader">
+            <span clss ="loader_text">Loading...</span>
+          </div>
+        : movies.map(movie =>(
+            <Movie id={movie.id} // props들 가져오기
+              key={movie.id}
+              year={movie.year}
+              title={movie.title}
+              summary={movie.summary}
+              poster={movie.medium_cover_image}/>
+              ))}
+      </section>
      );
   }
 }
