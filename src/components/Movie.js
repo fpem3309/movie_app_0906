6bc418987a6bbf91d2ca4dbd7131185d66780ed9
fpem3309 <alsrhks2154@gmail.com>
2021-09-14 17:30:04 +0900

    add detail

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,24 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import { Link } from "react-router-dom";
 import './Movie.css';
 
 function Movie({year,title,summary,poster, genres}){
         //App.js movies안의 movie
-    return <div className="movie">
-            <img src={poster} alt={title} title={title}/>
+    return (
+        <Link to={{
+            pathname:"/movie-detail",
+            state:{ // year : year 로 해도 된다.
+                year,
+                title,
+                summary,
+                poster,
+                genres
+            }
+        }}
+        >
+            <div className="movie">
+                <img src={poster} alt={title} title={title}/>
                 <div className="movie_data">
                     <h3 className="movie_title">{title} </h3>
                     <h5 className="movie_year">{year}</h5>
@@ -17,6 +30,8 @@ function Movie({year,title,summary,poster, genres}){
                     <p className="movie_summary">{summary.slice(0,140)}...</p>
                 </div>
            </div>
+           </Link>
+    );
 }
 
 Movie.propTypes = {
